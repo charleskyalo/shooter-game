a1632c101131ea075a038cf78c0c968baa74be8f
charles kyalo <charleskyalo689@gmail.com>
2020-10-08 12:19:49 +0300

    add player to the canvas

--- app.js
+++ app.js
@@ -5,4 +5,33 @@ const canvas = document.querySelector("canvas");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-console.log(canvas ) 
+console.log(canvas)
+
+const canvasContext = canvas.getContext('2d');
+
+
+class Player { 
+    constructor(x,y,radius,color) { 
+        this._x = x;
+        this._y = y;
+        this._radius = radius;
+        this._color = color;
+    }
+
+    draw() { 
+        canvasContext.beginPath();
+        canvasContext.arc(this._x, this._y, this._radius, 0, Math.PI * 2, false)
+        canvasContext.fillStyle = this._color;
+        canvasContext.fill();
+    }
+}
+
+// make the payer center
+const centerY = canvas.height / 2;
+const centerX = canvas.width / 2;
+
+
+
+const player = new Player(centerX, centerY, 30, "#737373");
+player.draw();
+console.log(player);
