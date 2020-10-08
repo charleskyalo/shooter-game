6f2227c18f3802c628f45839faa36fdadb6f8495
charles kyalo <charleskyalo689@gmail.com>
2020-10-08 11:52:34 +0300

    add starter files

---
+++ app.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("canvas");
+
+
+// give the canvas the full height and width of the viewport
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+console.log(canvas ) 
